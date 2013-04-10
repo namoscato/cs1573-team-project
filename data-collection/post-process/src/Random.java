3db496708d6067a7aec6eee8ec8abea62ae82b78
Nicholas Amoscato <nicholas@amoscato.com>
2013-04-10 18:41:35 -0400

    Count unrated movies

--- data-collection/post-process/src/Random.java
+++ data-collection/post-process/src/Random.java
@@ -10,10 +10,17 @@ public class Random {
 		try {
 			File input = new File("../clean_data.txt");
 			Scanner scanner = new Scanner(input);
+			int count = 0;
 			while (scanner.hasNextLine()) {
-				List<String> example = Arrays.asList(scanner.nextLine().split("\t"));
-				System.out.println(example.get(2) + "," + example.get(14));
+				String str = scanner.nextLine();
+				List<String> example = Arrays.asList(str.split("\t"));
+				if (example.get(10).equals("unrated")) {
+					//System.out.println(example.get(2) + "," + example.get(14));
+					count++;
+					System.out.println(str);
+				}
 			}
+			System.out.println(count);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
